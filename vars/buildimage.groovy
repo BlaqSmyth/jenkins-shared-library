71025bd9e201301a69ede04d745561f9515599de
BlaqSmyth <smithegharevba2@gmail.com>
2024-11-21 12:22:58 +0000

    update

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t BlaqSmyth/demo-app:jma-4.0 .'
+        sh 'docker build -t blaqsmyth/demo-app:jma-4.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push BlaqSmyth/demo-app:jma-4.0'
+        sh 'docker push blaqsmyth/demo-app:jma-4.0'
     }
 }
